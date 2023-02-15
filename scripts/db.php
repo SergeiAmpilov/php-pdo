3d5df89de43a615fd35b1c648b61c87896647352
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-02-15 20:04:16 +0300

    feat: pdo connection ready

--- scripts/db.php
+++ scripts/db.php
@@ -1,6 +1,18 @@
 <?php
 
-$host = getEnvParam('HOST') ?: 'localhost';
-$db = getEnvParam('HOST122') ?: 'my_db';
+$host   = getEnvParam('HOST') ?: 'localhost';
+$db   = getEnvParam('HOST122') ?: 'my_db';
 $user = getEnvParam('USER') ?: 'root';
-$user = getEnvParam('PASS') ?: '';
+$pass = getEnvParam('PASS') ?: '';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+var_dump($pdo);
